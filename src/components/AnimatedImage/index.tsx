147b7aedc45a1937b550de61c56344e2967895f0
zhavoronskaya <helenheykey@gmail.com>
2023-12-29 19:13:35 +0000

    fix(light_mode): outline color

--- src/components/AnimatedImage/index.tsx
+++ src/components/AnimatedImage/index.tsx
@@ -5,6 +5,7 @@ import Image from "next/image";
 import useScrolling from "../Scrolling/useScrolling";
 import { useRef } from "react";
 import useVisibilityPercentage from "@/hooks/useVisibilityPercentage";
+import { useTheme } from "@/context/ThemeContext";
 
 type Props = {
   src: string;
@@ -42,6 +43,9 @@ const AnimatedImage = ({
   const imgRect = imageRef.current?.getBoundingClientRect();
   const percentage = useVisibilityPercentage(imgRect);
 
+  const { theme } = useTheme();
+  const outlineColor =
+    theme === "dark" ? "outline-[#201F36]" : "outline-[#B0C0C2]";
   return (
     <Image
       ref={imageRef}
@@ -49,11 +53,7 @@ const AnimatedImage = ({
       width={width}
       height={height}
       alt={alt ?? ""}
-      className={
-        "w-full h-full object-cover rounded-lg  outline-[12px] outline-[#201F36] outline" +
-        " " +
-        imageClassName
-      }
+      className={`w-full h-full object-cover rounded-lg  outline-[12px] outline ${outlineColor} ${imageClassName}`}
       style={{
         objectPosition: `50% ${100 - percentage / positionPercentageDivider}%`,
       }}
